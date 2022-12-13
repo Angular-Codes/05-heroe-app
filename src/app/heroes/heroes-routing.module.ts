430b13a71ff976a0a6597e0149717430499302ce
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-12-12 22:30:46 -0500

    children routes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddComponent } from './pages/add/add.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SearchComponent } from './pages/search/search.component';
 
 const routes : Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'list',
