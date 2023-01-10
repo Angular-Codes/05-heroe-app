f1852cbf3b9d045085a9bd8d4f59d1d231da725c
Kevin Cuadros <kevindavidcuadros@gmail.com>
2023-01-10 17:30:56 -0500

    auth service

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +10,25 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
   onLogin(){
-    this.router.navigate(['./heroes'])
+    this.authService
+        .login()
+        .subscribe({
+          next: (value) => {
+            value && this.router.navigate(['./heroes'])
+            this.authService.setUser(value)
+          },
+          error: (err) => {
+            
+          },
+        })
   }
 
 }
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl
+
+  private user: Auth | undefined
+
+  get userData () {
+    return { ...this.user };
+  }
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  login(): Observable<Auth>{
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+  }
+
+  setUser(user: Auth) {
+    this.user = user;
+  }
+
+}
